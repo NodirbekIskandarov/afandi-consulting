388faf577a7fd15dbd2348c880a76a35314cb252
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-09-16 17:48:52 +0500

    chala footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from'./footer.module.scss'
+import footerimg from '../../assets/images/footerimg.png'
 function Footer() {
   return (
     <div className={styles.footer}>
-        
+        <div className={styles.container}>
+          <div className={styles.logo_part}>
+            <img src={footerimg} alt="footerlogo" />
+            <span>Afandi consulting offers online training in Environmental and Holistic Wellness.</span>
+          </div>
+
+          <div className={styles.menu}>
+            <div className={styles.company}>
+
+            </div>
+            <div className={styles.country}>
+
+            </div>
+            <div className={styles.social}>
+
+            </div>
+          </div>
+
+          <div className={styles.line}></div>
+        </div>
     </div>
   )
 }
