ff688923884dee4519185da7b7ca229a87a9b6cb
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-09-16 17:31:18 +0500

    ready contact part for desktop version

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './contact.module.scss'
+function Contact() {
+    return (
+        <div className={styles.contact}>
+            <div className={styles.title}>
+                <p>
+                    Contact us
+                </p>
+                <span>
+                    If you ever have questions, concerns, or just want to share your thoughts, please don’t hesitate to reach out to us. We’re all about open communication and value your feedback—it helps us make our courses and your experience even better! Your queries will be resolved promptly, usually within 24-48 hours.
+                </span>
+            </div>
+
+            <div className={styles.input_part}>
+                <input type="text" placeholder='First name'/>
+                <input type="text" placeholder='Last name'/>
+                <input type="text" placeholder='Phone number'/>
+                <textarea name="" id="" placeholder='Your message'>
+
+                </textarea>
+            </div>  
+        </div>
+    )
+}
+
+export default Contact
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Banner} from './banner/Banner'
 export {default as About} from './about/About'
 export {default as Country} from './country/Country'
+export {default as Contact} from './contact/Contact'
