f85e477809ef631126f44c648a47165d403913bb
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-09-18 15:38:02 +0500

    responsives added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { About, Banner, Contact, Country } from '../components'
-
+import styles from './home.module.scss'
 function Home() {
   return (
-    <div style={{
-      width: "1400px",
-      margin: "0 auto"
-    }}>
-      {/* <Banner/>
+    <div className={styles.home}>
+      <Banner/>
       <About />
       <Country/>
-      <Contact/> */}
+      <Contact/>
     </div>
   )
 }
