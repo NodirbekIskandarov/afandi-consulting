b65cbff5e60bf5c2d9e7c4b8a133486749c4b7c0
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-09-23 19:52:00 +0500

    .

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from'./footer.module.scss'
 import footerimg from '../../assets/images/footerimg.png'
 import { Link, useNavigate } from 'react-router-dom'
+import { getRequest } from '../../utils/request'
+import { country, sociallist } from '../../utils/API_urls'
 function Footer() {
   const navigate = useNavigate()
+  const [data, setData] = useState()
+  const [countryList, setCountryList] = useState()
   const handleMenuClick = (section) => {
     navigate(`/#${section}`);  // URL ga fragment (#about, #team) qo‘shib yo‘naltiramiz
   };
+  useEffect(() => {
+    getRequest(sociallist)
+    .then(response => {
+      setData(response?.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  useEffect(() => {
+    getRequest(country)
+    .then(response => {
+      setCountryList(response?.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  function navigateFunc(id) {
+    localStorage.setItem('uuid', id);
+    navigate('/details/jobs')
+  }
   return (
     <div className={styles.footer}>
         <div className={styles.container}>
           <div className={styles.logo_part}>
-            <img src={footerimg} alt="footerlogo" />
+            <a href="/"><img src={footerimg} alt="footerlogo" /></a>
             <span>Afandi consulting offers online training in Environmental and Holistic Wellness.</span>
           </div>
 
@@ -25,17 +53,23 @@ function Footer() {
             </div>
             <div className={styles.country}>
               <span>Country</span>
-              <Link className={styles.link} to="/">Uk</Link>
-              <Link className={styles.link} to="/">Austrial</Link>
-              <Link className={styles.link} to="/">Irlandy</Link>
-              <Link className={styles.link} to="/">United States</Link>
-              <Link className={styles.link} to="/">Canada</Link>
+              {
+                countryList?.slice(0, 4)?.map((item, index) => {
+                  return (
+                    <span key={index} className={styles.link} onClick={() => navigateFunc(item?.uuid)}>{item?.name}</span>
+                  )
+                })
+              }
             </div>
             <div className={styles.social}>
               <span>Social</span>
-              <Link className={styles.link} to="/">Facebook</Link>
-              <Link className={styles.link} to="/">Instagram</Link>
-              <Link className={styles.link} to="/">Linkedin</Link>
+              {
+                data?.map((item, index) => {
+                  return (
+                    <Link className={styles.link} to={item?.link} key={index}>{item?.name}</Link>
+                  )
+                })
+              }
             </div>
           </div>
 
